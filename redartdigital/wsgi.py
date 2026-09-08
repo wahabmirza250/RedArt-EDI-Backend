9de3da8861f797b4b1bb78e7ebb3306685c097c2
wahabmirza250 <aarizmirza605@gmail.com>
2026-09-07 18:24:38 -0600

    Run one-shot 999 diagnostic during temporary startup

--- redartdigital/wsgi.py
+++ redartdigital/wsgi.py
@@ -11,6 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+from redartdigital.ops_999_startup import run_once
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "redartdigital.settings.production")
 
+run_once()
 application = get_wsgi_application()
